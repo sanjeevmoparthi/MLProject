f854e88c52dfd0b7187e69888f1426a2420e93dd
sanjeevmoparthi <kumarsanjeev7101@gmail.com>
2025-05-20 12:27:29 +0530

    transform and predict checking

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,19 +2,30 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+import os
+
+
+
+
+
+
 
 class PredictPipeline:
     def __init__(self):
         pass
     def predict(self,features):
         try:
-            model_path = 'artifacts/model.pkl'
-            preprocessor_path = 'artifacts/preprocessor.pkl'
-            model = load_object(file_path = model_path)
-            preprocessor= load_object(file_path = preprocessor_path)
-            data_scaled = preprocessor.transform(features)
-            preds = model.predict(data_scaled)
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
             return preds
+           
+            
         except Exception as e:
             raise CustomException(e,sys)
 
@@ -22,7 +33,7 @@ class PredictPipeline:
 class CustomData:
     def __init__(  self,
                  gender:str,
-                 race_ethinicity:int,
+                 race_ethnicity:int,
                  parental_level_of_education,
                  lunch:str,
                  test_preparation_course:str,
@@ -30,7 +41,7 @@ class CustomData:
                  writing_score:int):
         
         self.gender = gender
-        self.race_ethinicity = race_ethinicity
+        self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
         self.lunch = lunch
         self.test_preparation_course = test_preparation_course
@@ -42,7 +53,7 @@ class CustomData:
         try:
             custom_data_input_dict={
                 "gender":[self.gender],
-                "race_ethinicity":[self.race_ethinicity],
+                "race_ethnicity":[self.race_ethnicity],
                 "parental_level_of_education":[self.parental_level_of_education],
                 "lunch":[self.lunch],
                 "test_preparation_course":[self.test_preparation_course],
